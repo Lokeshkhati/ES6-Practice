12df915f673f53134f23e955cb908a5bedfd83c1
Lokesh Khati <lokeshkhati005@gmail.com>
2023-03-14 10:14:37 +0530

    feat : add question 03 solution

---
+++ FP/Set-4/Question-03.js
@@ -0,0 +1,14 @@
+// Write an ES6 function that calculates and returns the total value of all items in an array of objects.
+
+const items = [
+    { name: "Item 1", price: 10 },
+    { name: "Item 2", price: 20 },
+    { name: "Item 3", price: 30 }
+];
+
+const totalValue = items => items.reduce((sum, item) => sum + item.price, 0)
+
+
+
+console.log(totalValue(items));
+  // Output: 60
