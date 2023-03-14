b11dc255cbdce831448f6f808f3c0ab46b5d2084
Lokesh Khati <lokeshkhati005@gmail.com>
2023-03-14 10:12:06 +0530

    feat : add question 02 solution

---
+++ FP/Set-4/Question-02.js
@@ -0,0 +1,10 @@
+// Write an ES6 function sumSquares that takes an array of numbers and returns the sum of the squares of all the numbers in the array
+
+const numbers = [1, 2, 3, 4, 5];
+// Your code here
+
+
+const sumSquares = numbers => numbers.reduce((sum, number) => sum + number * number,0)
+
+console.log(sumSquares(numbers));
+ // Output: 55
