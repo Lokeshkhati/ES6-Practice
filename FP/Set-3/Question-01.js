fcbe849b2773d1623c13927771131bd1bfb5c147
Lokesh Khati <lokeshkhati005@gmail.com>
2023-03-14 08:50:03 +0530

    refactor: refactor directory

---
+++ FP/Set-3/Question-01.js
@@ -0,0 +1,2 @@
+// Write an ES6 function that takes an array of objects representing cars with properties make, model, and year. Return the first car object that is a Toyota and the year is after 2010.
+
