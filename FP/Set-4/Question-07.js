09dc1c6f7790ddcc1bc2048d155f7b85f4bc87d7
Lokesh Khati <lokeshkhati005@gmail.com>
2023-03-14 11:23:46 +0530

    refactor : refactor code

--- FP/Set-4/Question-07.js
+++ FP/Set-4/Question-07.js
@@ -1,16 +1,14 @@
 // Write an ES6 function that takes an array of objects with name and age property, and returns the name of the oldest person.
 
-// const people = [
-//     { name: 'Jeena', age: 25 },
-//     { name: 'Priya', age: 30 },
-//     { name: 'Naina', age: 45 }
-// ]
+const people = [
+    { name: 'Jeena', age: 25 },
+    { name: 'Priya', age: 30 },
+    { name: 'Naina', age: 45 }
+]
 
-// const oldestPersonName = people => people.reduce((oldestPerson, person) => oldestPerson.age > person.age ? oldestPerson.name : person.name)
+const oldestPersonName = people => people.reduce((oldestPerson, person) => oldestPerson.age > person.age ? oldestPerson.name : person.name)
 
 
 
-// console.log(oldestPersonName(people));
+console.log(oldestPersonName(people));
 // Output: 'Naina'
-
-// console.log(biggestNum);
