4b39168dd79abcda660103780f2d67d32378b610
Lokesh Khati <lokeshkhati005@gmail.com>
2023-03-14 10:04:55 +0530

    feat : add quesion 01 solution

---
+++ FP/Set-4/Question-01.js
@@ -0,0 +1,9 @@
+// Given an array, write an ES6 function that returns the total length of all the strings in an array.
+
+const strings = ["apple", "banana", "cherry", "date", "blueberry"];
+
+const totalLength = strings => strings.reduce((totalLength, string) => totalLength + string.length, 0)
+
+
+console.log(totalLength(strings));
+// Output: 30
