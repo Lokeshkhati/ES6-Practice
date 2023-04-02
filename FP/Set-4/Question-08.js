991acf513d56988858e48bf3db22ea709a1ba4f0
Lokesh Khati <lokeshkhati005@gmail.com>
2023-04-02 18:32:53 +0530

    refactor : refactor code

--- FP/Set-4/Question-08.js
+++ FP/Set-4/Question-08.js
@@ -1,14 +1,18 @@
 // Write an ES6 function that takes an array of objects representing people with properties name and age, and returns an object with the average age of all the people.
 
 const people = [
-    { name: 'Alice', age: 25 },
-    { name: 'Bob', age: 30 },
-    { name: 'Charlie', age: 35 },
-    { name: 'David', age: 40 }
+  { name: 'Alice', age: 25 },
+  { name: 'Bob', age: 30 },
+  { name: 'Charlie', age: 35 },
+  { name: 'David', age: 40 }
 ];
 
 const peopleLength = people.length
-const getAverageAge = people => people.reduce((acc, curr) => acc + curr.age, 0) / peopleLength
+
+const getAverageAge = people => {
+  const averageAge = people.reduce((acc, curr) => acc + curr.age, 0) / peopleLength
+  return { averageAge }
+}
 
 
 console.log(getAverageAge(people));
