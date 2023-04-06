91beaecccb84fdbb0aabb8be96dca8b50354abbe
Lokesh Khati <lokeshkhati005@gmail.com>
2023-04-06 14:27:42 +0530

    feat : add solution of exercise 06

---
+++ Reactjs/Set-04/src/components/Question06.jsx
@@ -0,0 +1,34 @@
+// a. Build a React component that displays the title and author of each book.
+// b. Add buttons to filter the books by genre using the useState hook.
+
+import { useState } from "react";
+import { booksData, genres } from "../constants/books";
+const BookCard = () => {
+  const [genre, setGenre] = useState("");
+
+  const filteredBooks =
+    genre === "All"
+      ? booksData
+      : booksData.filter((book) => book.genre === genre);
+
+  return (
+    <div>
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+      </div>
+      <ul>
+        {filteredBooks.map(({ id, title, genre, author }) => (
+          <li>
+            <h2>{title} </h2>
+            <p>{author} </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default BookCard;
