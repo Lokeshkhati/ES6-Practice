2d2b763762923f1411e354e8e442f5f9c392aa2f
Lokesh Khati <lokeshkhati005@gmail.com>
2023-04-07 12:05:52 +0530

    feat : add data & solution

--- Reactjs/Set-05/src/App.jsx
+++ Reactjs/Set-05/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import UserCard from "./components/UserCard";
+import ProductCard from "./components/ProductCard";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div className="App">
-      <UserCard />
+      {/* <UserCard /> */}
+      {/* <ProductCard /> */}
+      <UserProfile heading="User Profile" width={400} height={300} />
     </div>
   );
 }
