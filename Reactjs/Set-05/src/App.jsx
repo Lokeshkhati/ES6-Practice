60146fdbb6653c6605c19416213c6dca9d0a468b
Lokesh Khati <lokeshkhati005@gmail.com>
2023-04-07 13:24:19 +0530

    feat : add solution of question 07

--- Reactjs/Set-05/src/App.jsx
+++ Reactjs/Set-05/src/App.jsx
@@ -5,6 +5,7 @@ import ProductCard from "./components/ProductCard";
 import UserProfile from "./components/UserProfile";
 import UserFeed from "./components/UserFeed";
 import Chat from "./components/Chat";
+import Comments from "./components/Comments";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <ProductCard /> */}
       {/* <UserProfile heading="User Profile" width={400} height={300} /> */}
       {/* <UserFeed /> */}
-      <Chat />
+      {/* <Chat /> */}
+      <Comments />
     </div>
   );
 }
---
+++ Reactjs/Set-05/src/components/Comments.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { fakeFetch, url } from "../constants/fakeFetch06";
+const Comments = () => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const getComments = async (url) => {
+    setIsLoading(true);
+    try {
+      const { data } = await fakeFetch(url);
+      setComments(data.comments);
+      setError("");
+    } catch (error) {
+      setError(error.message);
+      setComments([]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getComments(url);
+  }, []);
+
+  const deleteHandler = (name) => {
+    const updatedComments = comments.filter((comment) => comment.name !== name);
+    setComments(updatedComments);
+  };
+  return (
+    <div>
+      {error && <h1>{error} </h1>}
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          {comments.map(({ name, text }) => (
+            <>
+              <h1>{name} </h1>
+              <p>{text} </p>
+              <button onClick={() => deleteHandler(name)}>Delete</button>
+            </>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+export default Comments;
