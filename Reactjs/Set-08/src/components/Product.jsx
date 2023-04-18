5af5ef344f429781da56722b072411b241d0ffc0
lokesh <lokeshkhati005@gmail.com>
2023-04-18 17:45:45 +0530

    add solution

--- Reactjs/Set-08/src/components/Product.jsx
+++ Reactjs/Set-08/src/components/Product.jsx
@@ -14,11 +14,18 @@ const Product = ({ product }) => {
   };
 
   const isInCart = cart.find((product) => product.id === id);
+  const isInWishlist = wishlist.find((product) => product.id === id);
 
   const removeFromCart = (id) => {
     const updatedCart = cart.filter((product) => product.id !== id);
     setCart(updatedCart);
   };
+  const removeFromWishlist = (id) => {
+    const updatedCart = wishlist.filter((product) => product.id !== id);
+    setWishlist(updatedCart);
+  };
+
+
   return (
     <div>
       <li className="product-card">
@@ -40,22 +47,29 @@ const Product = ({ product }) => {
         </p>
 
         <Link to={`/product/${id}`}>visit item</Link>
-        {/* {isInCart ? (
+        
+        {isInCart ? (
           <button className="cart-btn" onClick={() => removeFromCart(id)}>
             Remove from cart
           </button>
-        ) : ( */}
-        <button
-          className="cart-btn"
-          disabled={isInCart}
-          onClick={() => addToCart(product)}
-        >
-          Add to cart
-        </button>
-        {/* )} */}
-        <button className="cart-btn" onClick={() => addToWishlist(product)}>
-          Add to Wishlist
-        </button>
+        ) : (
+          <button
+            className="cart-btn"
+            disabled={isInCart}
+            onClick={() => addToCart(product)}
+          >
+            Add to cart
+          </button>
+        )}
+        {isInWishlist ? (
+          <button className="cart-btn" onClick={() => removeFromWishlist(id)}>
+            Remove from Wishlist
+          </button>
+        ) : (
+          <button className="cart-btn" onClick={() => addToWishlist(product)}>
+            Add to Wishlist
+          </button>
+        )}
       </li>
     </div>
   );
