134e70e991271bd2e6de8171bfc2c5148b74c082
lokesh <lokeshkhati005@gmail.com>
2023-04-26 22:16:07 +0530

    refactor code

--- FP/Set-4/Question-10.js
+++ FP/Set-4/Question-10.js
@@ -14,26 +14,14 @@ const fruits = ['apple', 'banana', 'banana', 'cherry', 'apple', 'apple', 'banana
 // }, {})
 
 // solution 2
-const countStrings = strings => strings.reduce((acc, fruit) => {
-    return { ...acc, [fruit]: (acc[fruit] || 0) + 1 }
+const countStrings = strings => strings.reduce((acc, curr) => {
+    return { ...acc, [curr]: (acc[curr] || 0) + 1 }
 }, {})
 
 
 console.log(countStrings(fruits));
 
-// another solution without reduce
-// let count = {}
-// for (const fruit of fruits) {
-//     console.log(count[fruit]);
-//     if (count[fruit]) {
-//         count[fruit] += 1
-//     }
-//     else {
-//         count[fruit] = 1
-//     }
 
 
-// }
-// console.log(count);
 
 
