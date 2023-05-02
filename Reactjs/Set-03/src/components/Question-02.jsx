1a8c385a1de041ea925d5c8edd2046f1b18cb851
lokesh <lokeshkhati005@gmail.com>
2023-05-02 22:01:09 +0530

    refactor

--- Reactjs/Set-03/src/components/Question-02.jsx
+++ Reactjs/Set-03/src/components/Question-02.jsx
@@ -1,6 +1,5 @@
 // Given an array of objects with cartoon character details as: id, name, superpower, magnitude. Build a React component and display all details of those characters whose magnitude is greater than 5. Data:
 import { cartoons } from "../utils/cartoons";
-
 const Cartoons = () => {
   return (
     <div>
